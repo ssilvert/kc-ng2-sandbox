4a04504b02617016f31d94bf950d97ade111b572
Stan Silvert <ssilvert@redhat.com>
2016-10-28 13:16:58 -0400

    Update realm setup

--- src/main/webapp/app/keycloak.service.ts
+++ src/main/webapp/app/keycloak.service.ts
@@ -4,45 +4,54 @@ declare var Keycloak: any;
 
 @Injectable()
 export class KeycloakService {
-  static auth: any = {};
+    static auth: any = {};
+    static keycloakAuth: any = new Keycloak('keycloak.json');
 
-  static init(): Promise<any> {
-    let keycloakAuth: any = new Keycloak('keycloak.json');
-    KeycloakService.auth.loggedIn = false;
+    static init(): Promise<any> {
+        KeycloakService.auth.loggedIn = false;
 
-      return new Promise((resolve, reject) => {
-        keycloakAuth.init({ onLoad: 'login-required' })
-          .success(() => {
-            KeycloakService.auth.loggedIn = true;
-            KeycloakService.auth.authz = keycloakAuth;
-            KeycloakService.auth.logoutUrl = keycloakAuth.authServerUrl 
-                + "/realms/klient-realm/protocol/openid-connect/logout?redirect_uri=http://localhost:3000/";
-            resolve();
-          })
-          .error(() => {
-            reject();
-          });
-      });
+        return new Promise((resolve, reject) => {
+            KeycloakService.keycloakAuth.init({ onLoad: 'login-required' })
+                .success(() => {
+                    console.log("------------------------------");
+                    console.log(`authServerUrl=${KeycloakService.keycloakAuth.authServerUrl}`);
+                    console.log(`realm=${KeycloakService.keycloakAuth.realm}`);
+                    console.log(`resource=${KeycloakService.keycloakAuth.resource}`);
+                    console.log(`publicClient=${KeycloakService.keycloakAuth.publicClient}`);
+                    console.log(`subject=${KeycloakService.keycloakAuth.subject}`);
+                    console.log(`realmAccess=${JSON.stringify(KeycloakService.keycloakAuth.realmAccess)}`);
+                    console.log(`idTokenParsed=${JSON.stringify(KeycloakService.keycloakAuth.idTokenParsed)}`);
+                    console.log(`logoutUrl=${KeycloakService.keycloakAuth.createLogoutUrl()}`);
+                    console.log("------------------------------");
+
+                    KeycloakService.auth.loggedIn = true;
+                    KeycloakService.auth.authz = KeycloakService.keycloakAuth;
+                    KeycloakService.auth.logoutUrl = KeycloakService.keycloakAuth.createLogoutUrl();
+                    resolve();
+                })
+                .error(() => {
+                    reject();
+                });
+        });
     }
 
-  logout() {
-    KeycloakService.auth.loggedIn = false;
-    KeycloakService.auth.authz = null;
+    logout() {
+        KeycloakService.auth.loggedIn = false;
+        KeycloakService.auth.authz = null;
+        KeycloakService.keycloakAuth.logout();
+    }
 
-    window.location.href = KeycloakService.auth.logoutUrl;
-  }
-
-  getToken(): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-      if (KeycloakService.auth.authz.token) {
-        KeycloakService.auth.authz.updateToken(5)
-          .success(() => {
-            resolve(<string>KeycloakService.auth.authz.token);
-          })
-          .error(() => {
-            reject('Failed to refresh token');
-          });
-      }
-    });
-  }
+    getToken(): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            if (KeycloakService.auth.authz.token) {
+                KeycloakService.auth.authz.updateToken(5)
+                    .success(() => {
+                        resolve(<string>KeycloakService.auth.authz.token);
+                    })
+                    .error(() => {
+                        reject('Failed to refresh token');
+                    });
+            }
+        });
+    }
 }
